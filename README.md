c3ec3819a8b7b14f9bdac68061aa41788e99bde2
davidevinavil <davidevinavil@gmail.com>
2013-05-27 22:56:28 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+kernel_s500_jb
+==============
+
+Kernel source for Acer CloudMobile 
+
+Use Google gcc 4.7 to build this source.
---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 24 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#24 SMP PREEMPT Fri May 24 13:23:27 CEST 2013"
+#define LINUX_COMPILE_BY "Davide"
+#define LINUX_COMPILE_HOST "D M"
+#define LINUX_COMPILER "gcc version 4.7 (GCC) "
