abfdd0256ab314d44563facba946bd8c954697e4
spacecaker <robin-barry@hotmail.com>
2013-12-28 20:15:58 +0100

    Add Some MSM_FB fixes and add CPU_VOLTAGE_TABLE and  enable ksm and some more features in defconfig

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Sat Dec 28 19:32:29 CET 2013"
+#define LINUX_COMPILE_BY "SpaceCaker"
+#define LINUX_COMPILE_HOST "MSR-K"
+#define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
